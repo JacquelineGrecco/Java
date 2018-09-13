eca49ddf7f4a2e4a3f837400927ae400c043d138
JacquelinePires <jacqueline.pires@youse.com.br>
2018-09-13 20:13:32 -0300

    Resolving conflicts' EOF ;

---
+++ sintaxe-basica/src/TestaCaracteres.java
@@ -0,0 +1,15 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char letra = 'a'; 
+		System.out.println(letra);
+
+		String frase = "Olá mundo!";
+		System.out.println(frase);
+		
+		frase = frase + 2020; 
+		System.out.println(frase);
+	}	
+}
